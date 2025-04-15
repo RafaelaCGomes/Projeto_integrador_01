1b6c394b9083ba047c68d4fbafd6eeb358838549
RafaelaCGomes <rafa.carolyne.gomes@gmail.com>
2025-04-15 20:43:59 -0300

    atualização

--- framework_web/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists posts;
-
-create table posts(
-    id integer primary key autoincrement,
-    created timestamp not null default current_timestamp,
-    title text not null,
-    content text not null
-);
