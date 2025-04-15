113608586a1410279e7201bb7a360daeaf491151
RafaelaCGomes <rafa.carolyne.gomes@gmail.com>
2025-04-15 20:41:35 -0300

    apagando tudo

--- framework_web/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists posts;
-
-create table posts(
-    id integer primary key autoinclement,
-    created timestamp not null default current_timestamp,
-    title text not null,
-    content text not null
-);
