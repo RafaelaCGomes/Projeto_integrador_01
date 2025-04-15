3dc2645c5a6106d9d4a27095e1f736ce0c34b72d
Rafaela Caroline Gomes <rafa.carolyne.gomes@gmail.com>
2025-04-14 21:00:29 -0300

    Update schema.sql

--- framework_web/schema.sql
+++ framework_web/schema.sql
@@ -1,7 +1,7 @@
 drop table if exists posts;
 
 create table posts(
-    id integer primary key autoinclement,
+    id integer primary key autoincrement,
     created timestamp not null default current_timestamp,
     title text not null,
     content text not null
