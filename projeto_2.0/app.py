beba64d4c976f08370a56ba879efc62dc0e20b52
RafaelaCGomes <rafa.carolyne.gomes@gmail.com>
2025-05-08 16:45:24 -0300

    atualização

--- projeto_2.0/app.py
+++ projeto_2.0/app.py
@@ -20,6 +20,8 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 # Configuração da pasta de upload
 app.config['UPLOAD_FOLDER_EVENTOS'] = os.path.join('static', 'img', 'uploads')  # Pasta para eventos
 app.config['UPLOAD_FOLDER_GALERIA'] = os.path.join('static', 'img', 'galeria')  # Pasta para galeria
+app.config['UPLOAD_FOLDER_CONTAS'] = os.path.join('static', 'img', 'prest_contas')  # Pasta para prestação de contas
+
 
 # Função utilitária para verificar extensões de arquivos permitidos
 def allowed_file(filename):
@@ -44,6 +46,14 @@ class Galeria(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.String(200), nullable=False)
     image_filenames = db.Column(db.String(5000))  # Agora será uma string com os nomes das imagens separados por ';'
+
+#Tabela Prestação de contas
+class prestacaocontas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    image_filenames = db.Column(db.String(5000))
 
 # Modelo de Usuário
 class User(UserMixin, db.Model):
@@ -83,7 +93,8 @@ def doacao():
 
 @app.route('/prestacao_contas')
 def contas():
-    return render_template('prestacao_contas.html')
+    posts = prestacaocontas.query.all()
+    return render_template('prestacao_contas.html', posts=posts)
 
 @app.route('/contato')
 def contato():
@@ -207,3 +218,43 @@ def deletar_galeria(post_id):
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for('galeria'))
+
+@app.route('/novo_contas', methods=['GET', 'POST'])
+@login_required
+def novo_contas():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        images = request.files.getlist('images')  # Usando getlist para pegar múltiplos arquivos
+
+        image_filenames = []  # Lista para armazenar os nomes das imagens
+        for image in images:
+            if image and allowed_file(image.filename):
+                filename = secure_filename(image.filename)  # Garantir nome seguro
+                upload_path = os.path.join(app.config['UPLOAD_FOLDER_CONTAS'], filename)
+                print(f"Salvando imagem em: {upload_path}")  # Agora funciona corretamente
+                image.save(upload_path)
+                image_filenames.append(filename)
+
+        # Salvando os nomes das imagens como uma única string separada por ponto e vírgula
+        new_post = prestacaocontas(
+            title=title, 
+            content=content, 
+            image_filenames=";".join(image_filenames)  # Salva as imagens concatenadas por ponto e vírgula
+        )  
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('contas'))
+
+    return render_template('novo_contas.html')
+
+# Deletar imagem de contas
+@app.route('/deletar_contas/<int:post_id>', methods=['POST'])
+@login_required
+def deletar_contas(post_id):
+    post = prestacaocontas.query.get_or_404(post_id)  # Buscar na tabela Galeria
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('contas'))
+
